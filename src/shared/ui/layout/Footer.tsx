11927c9280512ceb0a57174298a36ccfd34c0bde
sam8e1ito <samvelsymonian@gmail.com>
2026-05-31 00:15:41 +0200

    bug: final fix of footer

--- src/shared/ui/layout/Footer.tsx
+++ src/shared/ui/layout/Footer.tsx
@@ -1,8 +1,8 @@
 export const Footer = () => {
     return (
         <footer>
-            <a href="/public/Impressum" download>Impressum</a>
-            <a href="/public/Datenschutz" download>Datenschutz</a>
+            <a href="/Impressum.pdf" download>Impressum</a>
+            <a href="/Datenschutz.pdf" download>Datenschutz</a>
         </footer>
     )
 }
